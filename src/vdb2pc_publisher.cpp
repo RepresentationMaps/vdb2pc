4e50b0f30394a9a818fead644742181cd0a063ac
Joe <jozsefpalmieri@hotmail.com>
2024-07-13 14:39:07 +0000

    Wip - The License type has been changed from Apache-2.0 to LGPLv3, and the noticing text has been properly modified.

--- src/vdb2pc_publisher.cpp
+++ src/vdb2pc_publisher.cpp
@@ -1,40 +1,31 @@
 /*********************************************************************
  *
  * Software License Agreement
- *
- *  Copyright (c) 2018, Simbe Robotics, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Simbe Robotics, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * Author: Steve Macenski (steven.macenski@simberobotics.com)
- * Purpose: convert native vdb files to pointclouds
+ * 
+ * Copyright (c) 2024, [Your Name/Organization]
+ * All rights reserved.
+ * 
+ * This file is part of VDB2PC, derived from [Original Project Name].
+ * Original work Copyright (c) 2018, Simbe Robotics, Inc.
+ * 
+ * Modifications Copyright (c) 2024, [Your Name/Organization]
+ * 
+ * This software is licensed under the terms of the LGPLv3 license.
+ * See the LICENSE file in the project root for more information.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * Authors: Name (email), Name (email)
+ * Purpose: convert VDB structures into PointCloud
+ * 
  *********************************************************************/
+
 #include "vdb2pc_publisher.hpp"
 
 using namespace vdb2pc::ros_utils;
